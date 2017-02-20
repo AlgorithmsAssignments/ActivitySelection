bb05112e21d6b8c6658dcb27a0351ce8d64b93b3
Turupawn <ahmed.hn.43@gmail.com>
2017-02-20 16:56:14 -0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Test.h"
+#include <iostream>
+using namespace std;
+
+vector<int> selectActivities(vector<int> start, vector<int> finish)
+{
+    vector<int> answer;
+    return answer;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
