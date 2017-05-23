19c52c704c161f15b29570bed4c2427ec8a0425b
Raim <rai.interiano@gmail.com>
2017-05-23 17:04:45 -0600

    Solved. Check if start[i] > finish[index] where index = answer.size() -1;

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@ using namespace std;
 vector<int> selectActivities(vector<int> start, vector<int> finish)
 {
     vector<int> answer;
+    answer.push_back(0);
+
+    for(unsigned int i = 0; i < start.size(); ++i)
+    {
+      unsigned int index = answer.size() - 1;
+      if(start[i] >= finish[index])
+      {
+        cout<<"Start: "<<start[i]<<endl;
+        cout<<"Finish: "<<finish[i]<<endl;
+        cout<<"Index: "<<index<<" and i: "<<i<<endl;
+        answer.push_back(i);
+      }
+    }
     return answer;
 }
 
